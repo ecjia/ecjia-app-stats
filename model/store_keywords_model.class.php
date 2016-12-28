83eb7a4b4fc0e725c074d1bffbca03de10a50475
靳翔 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-28 16:33:16 +0800

    迁移商家后台stats到平台后台

---
+++ model/store_keywords_model.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 商家关键字模型
+ */
+defined('IN_ECJIA') or exit('No permission resources.');
+class store_keywords_model extends Component_Model_Model {
+	public $table_name = '';
+	public $view = array();
+	public function __construct() {
+		$this->table_name = 'store_keywords';
+		parent::__construct();
+	}
+}
+
+// end
